9ce21c83c45948cb86cf1ba822ba2c5fb75cc250
rjclemens <71605742+rjclemens@users.noreply.github.com>
2021-08-16 22:55:21 -0500

    loading animation complete

---
+++ app.js
@@ -0,0 +1,18 @@
+const tl = gsap.timeline({defaults: {ease: 'power1.out'}});
+
+/*   elem-to-animate, {animation-to-complete, duration, stagger} */
+tl.to('.text', {y: "0%", duration: 1, stagger: 0.25});
+
+tl.to('.slider', {y: "-100%", duration: 1.5, delay: 0.25}); /* slide window upwards to fill view */
+tl.to('.intro', {y: "-100%", duration: 1.5}, "-=1.5"); /* start a second sooner */
+
+tl.fromTo('nav', {opacity: 0}, {opacity: 1, duration: 1});
+tl.fromTo('nav', {opacity: 0}, {opacity: 1, duration: 1}, "-=1");
+
+function onbutton(x){
+    x.style.color = "#DDCDE8";
+}
+
+function offbutton(x){
+    x.style.color = "black";
+}
